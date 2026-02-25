b90fcb91a8a544b7a23d162f164e8648eb381591
Snizhana <3836409641@cuspu.edu.ua>
2026-02-25 09:13:28 +0200

    new origin

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  //origin: ["http://localhost:5173"],
+  origin: ["https://time-capsule-ten-flame.vercel.app"],
   credentials: true
 }));
 
