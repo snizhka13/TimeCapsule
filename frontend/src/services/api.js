7a4d1a90670e430527f515b59f99f75596140c69
Snizhana <3836409641@cuspu.edu.ua>
2026-02-25 09:37:17 +0200

    updated api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,4 +5,15 @@ const api = axios.create({
   withCredentials: true
 });
 
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    if (!error.response) {
+      // retry once if server was sleeping
+      return api.request(error.config);
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
